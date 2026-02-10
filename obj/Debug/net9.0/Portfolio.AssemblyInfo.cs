f038e4116fb3666c809d38e09fc64e90095b0b6e
tkraft644 <kraft778@gmail.com>
2026-02-10 13:39:03 +0100

    Logo

--- obj/Debug/net9.0/Portfolio.AssemblyInfo.cs
+++ obj/Debug/net9.0/Portfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2cf42e9d8d1659002cc8346708ad176a3dcc09f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7be893feaf5061a24fcaaf83fb266c80b194871f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
